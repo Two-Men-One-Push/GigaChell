7039f047bc3d194c5655f22ccbcdbc42d0e473e6
CyberOneFR <erwoan.thebault@gmail.com>
2025-05-08 16:49:42 +0200

    norm error test

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ebini <ebini@student.42lyon.fr>            +#+  +:+       +#+        */
+/*   By: ethebaul <ethebaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 15:05:38 by ebini             #+#    #+#             */
-/*   Updated: 2025/05/08 15:28:51 by ebini            ###   ########lyon.fr   */
+/*   Updated: 2025/05/08 16:49:31 by ethebaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ int	main(int ac, char **av, char **envp)
 	line = readline(PROMPT);
 	while (line)
 	{
+		
 		status_code = exec_pipe(line);
 		free(line);
 		line = readline(PROMPT);
