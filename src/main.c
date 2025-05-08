c6b80b5fe33065d794f454dd4f5d5c865036d597
CyberOneFR <erwoan.thebault@gmail.com>
2025-05-08 16:59:35 +0200

    test norm error

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ethebaul <ethebaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 15:05:38 by ebini             #+#    #+#             */
-/*   Updated: 2025/05/08 16:57:34 by ethebaul         ###   ########.fr       */
+/*   Updated: 2025/05/08 16:59:25 by ethebaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ int	main(int ac, char **av, char **envp)
 	line = readline(PROMPT);
 	while (line)
 	{
+		
 		status_code = exec_pipe(line);
 		free(line);
 		line = readline(PROMPT);
