cbfd6a3310d5cd496f7879916bd566c305234c47
CyberOneFR <erwoan.thebault@gmail.com>
2025-05-08 16:57:41 +0200

    test good norm

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ethebaul <ethebaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 15:05:38 by ebini             #+#    #+#             */
-/*   Updated: 2025/05/08 16:49:31 by ethebaul         ###   ########.fr       */
+/*   Updated: 2025/05/08 16:57:34 by ethebaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@ int	main(int ac, char **av, char **envp)
 	line = readline(PROMPT);
 	while (line)
 	{
-		
 		status_code = exec_pipe(line);
 		free(line);
 		line = readline(PROMPT);
