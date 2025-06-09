b7c28858d1aef8fbbb9cbd3ba6f3cf982c7c9951
CyberOneFR <erwoan.thebault@gmail.com>
2025-06-09 17:27:07 +0200

    added expand.c

--- headers/utils.h
+++ headers/utils.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ebini <ebini@student.42lyon.fr>            +#+  +:+       +#+        */
+/*   By: ethebaul <ethebaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 00:57:14 by ebini             #+#    #+#             */
-/*   Updated: 2025/05/14 06:29:08 by ebini            ###   ########lyon.fr   */
+/*   Updated: 2025/06/09 17:18:10 by ethebaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ bool	skip_squote(char *s, size_t *i);
 char	*get_limiter(char *cmd, size_t *i);
 
 void	str_unquote(char *s);
+
+char	*expand(char *cmd);
 
 /**
  * @brief Open a temporary file with flag permission and returns its fd.
--- srcs/utils/expand.c
+++ srcs/utils/expand.c
@@ -3,12 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   expand.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ebini <ebini@student.42lyon.fr>            +#+  +:+       +#+        */
+/*   By: ethebaul <ethebaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 04:42:35 by ebini             #+#    #+#             */
-/*   Updated: 2025/05/14 04:43:03 by ebini            ###   ########lyon.fr   */
+/*   Updated: 2025/06/09 17:21:55 by ethebaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env.h"
+#include "libft.h"
+#include <stdlib.h>
 
+char	*expand(char *cmd)
+{
+	char	*var;
+	char	*result;
+	int		j;
+	int		i;
+
+	i = 0;
+	while (ft_isalnum(cmd[i]) || cmd[i] == '_')
+		++i;
+	var = malloc((i + 1) * sizeof(char));
+	j = 0;
+	while (j < i)
+	{
+		var[j] = cmd[j];
+		++j;
+	}
+	result = ft_getenv(var);
+	free(var);
+	return (result);
+}
