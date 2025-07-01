0cd01df583e73584beaed6d04feac11691933a90
ebini <ebini@student.42lyon.fr>
2025-07-01 21:48:14 +0200

    fix(echo): not greatly handling space before arguments when -n is in flags

--- srcs/builtin/echo/echo.c
+++ srcs/builtin/echo/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ethebaul <ethebaul@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ebini <ebini@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 17:36:18 by ethebaul          #+#    #+#             */
-/*   Updated: 2025/06/30 17:22:35 by ethebaul         ###   ########.fr       */
+/*   Updated: 2025/07/01 21:46:50 by ebini            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	ft_echo(int argc, char **argv, t_redirect_fd *redirect)
 	}
 	while (i < argc)
 	{
-		if (i != 1)
+		if (i != 1 + !newline)
 			ft_dprintf(redirect->out, " %s", argv[i]);
 		else
 			ft_dprintf(redirect->out, "%s", argv[i]);
