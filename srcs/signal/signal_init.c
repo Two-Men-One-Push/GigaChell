7d0e159c80e853933f8416fabe6f4d4941568579
ebini <ebini@student.42lyon.fr>
2025-07-17 00:55:17 +0200

    fix: function implementation types

--- srcs/signal/signal_init.c
+++ srcs/signal/signal_init.c
@@ -6,7 +6,7 @@
 /*   By: ebini <ebini@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 21:21:04 by CyberOneFR        #+#    #+#             */
-/*   Updated: 2025/07/16 23:40:41 by ebini            ###   ########lyon.fr   */
+/*   Updated: 2025/07/17 00:55:05 by ebini            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,62 +15,47 @@
 #include <readline/readline.h>
 #include "signal_handling.h"
 
-int	handling_prompt_signal(void)
+void	handling_prompt_signal(void)
 {
 	struct sigaction	sigint_sa;
 	struct sigaction	sigquit_sa;
 
 	sigint_sa.sa_flags = 0;
 	sigint_sa.sa_handler = &sigint_handler;
-	if (sigemptyset(&sigint_sa.sa_mask))
-		return (1);
-	if (sigaction(SIGINT, &sigint_sa, NULL))
-		return (1);
+	sigemptyset(&sigint_sa.sa_mask);
+	sigaction(SIGINT, &sigint_sa, NULL);
 	sigquit_sa.sa_flags = 0;
 	sigquit_sa.sa_handler = SIG_IGN;
-	if (sigemptyset(&sigquit_sa.sa_mask))
-		return (1);
-	if (sigaction(SIGQUIT, &sigquit_sa, NULL))
-		return (1);
-	return (0);
+	sigemptyset(&sigquit_sa.sa_mask);
+	sigaction(SIGQUIT, &sigquit_sa, NULL);
 }
 
-int	handling_execution_signal(void)
+void	handling_execution_signal(void)
 {
 	struct sigaction	sigint_sa;
 	struct sigaction	sigquit_sa;
 
 	sigint_sa.sa_flags = 0;
 	sigint_sa.sa_handler = SIG_IGN;
-	if (sigemptyset(&sigint_sa.sa_mask))
-		return (1);
-	if (sigaction(SIGINT, &sigint_sa, NULL))
-		return (1);
+	sigemptyset(&sigint_sa.sa_mask);
+	sigaction(SIGINT, &sigint_sa, NULL);
 	sigquit_sa.sa_flags = 0;
 	sigquit_sa.sa_handler = SIG_IGN;
-	if (sigemptyset(&sigquit_sa.sa_mask))
-		return (1);
-	if (sigaction(SIGQUIT, &sigquit_sa, NULL))
-		return (1);
-	return (0);
+	sigemptyset(&sigquit_sa.sa_mask);
+	sigaction(SIGQUIT, &sigquit_sa, NULL);
 }
 
-int	handling_child_signal(void)
+void	handling_child_signal(void)
 {
 	struct sigaction	sigint_sa;
 	struct sigaction	sigquit_sa;
 
 	sigint_sa.sa_flags = 0;
 	sigint_sa.sa_handler = SIG_DFL;
-	if (sigemptyset(&sigint_sa.sa_mask))
-		return (1);
-	if (sigaction(SIGINT, &sigint_sa, NULL))
-		return (1);
+	sigemptyset(&sigint_sa.sa_mask);
+	sigaction(SIGINT, &sigint_sa, NULL);
 	sigquit_sa.sa_flags = 0;
 	sigquit_sa.sa_handler = SIG_DFL;
-	if (sigemptyset(&sigquit_sa.sa_mask))
-		return (1);
-	if (sigaction(SIGQUIT, &sigquit_sa, NULL))
-		return (1);
-	return (0);
+	sigemptyset(&sigquit_sa.sa_mask);
+	sigaction(SIGQUIT, &sigquit_sa, NULL);
 }
