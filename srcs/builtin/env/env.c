3fcc13c0c2cf06aaa67e2832fb2fd9cd121bf71b
CyberOneFR <erwoan.thebault@gmail.com>
2025-07-17 03:17:32 +0200

    fixed env

--- srcs/builtin/env/env.c
+++ srcs/builtin/env/env.c
@@ -6,13 +6,14 @@
 /*   By: ethebaul <ethebaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 18:00:32 by ethebaul          #+#    #+#             */
-/*   Updated: 2025/06/30 18:13:02 by ethebaul         ###   ########.fr       */
+/*   Updated: 2025/07/17 03:16:39 by ethebaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env.h"
 #include "redirect_fd.h"
 #include "utils.h"
+#include <stdlib.h>
 
 int	ft_env(int argc, char **argv, t_redirect_fd	*redirect)
 {
@@ -24,5 +25,6 @@ int	ft_env(int argc, char **argv, t_redirect_fd	*redirect)
 	if (!env)
 		return (1);
 	dprint_tab(redirect->out, env);
+	free(env);
 	return (0);
 }
